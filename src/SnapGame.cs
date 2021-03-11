3644726c008053ff59aca6c684d5d8f51eae82e7
Samuel Liew <101228489@students.swinburne.edu.my>
2021-03-11 18:08:24 +0800

    changing UI - font

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -64,6 +64,7 @@ namespace CardGames
 			if (top != null)
 			{
 				SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.Red, "GameFont", 0, 20);
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.Red, "GameFont",0, 40); 
 				SwinGame.DrawText ("Player 1 score: " + myGame.Score(1), Color.Red, "GameFont",0, 60); 
 				SwinGame.DrawCell (SwinGame.BitmapNamed("Cards"), top.CardIndex, 521, 153);
